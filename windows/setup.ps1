eb08c9c114a4bd584a48c432f1a60f2dc980aac1
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-03-16 10:20:50 +0100

    feat: zed, docker, greenshot
    Added development dependencies and screenshot utilities

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -4,6 +4,7 @@ winget install --id Mozilla.Firefox.DeveloperEdition --source winget
 winget install --id Adobe.Acrobat.Reader.64-bit --source winget
 winget install --id Bitwarden.Bitwarden --source winget
 winget install --id 7zip.7zip --source winget
+winget install --id Greenshot.Greenshot --source winget
 
 # Research
 winget install --id JabRef.JabRef --source winget
@@ -12,7 +13,9 @@ winget install --id DigitalScholar.Zotero --source winget
 
 # Development
 winget install --id Git.Git --source winget
+winget install --id Docker.DockerDesktop --source winget
 winget install --id Microsoft.VisualStudioCode --source winget
+winget install --id ZedIndustries.Zed --source winget
 winget install --id JGraph.Draw --source winget
 winget install --id EclipseAdoptium.Temurin.25.JDK --source winget
 
