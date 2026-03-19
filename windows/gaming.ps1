d4169227e166c48f5dacb6150bdd9db291bf5bcb
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-03-19 10:45:59 +0100

    feat: add monitoring apps and links

--- windows/gaming.ps1
+++ windows/gaming.ps1
@@ -7,3 +7,13 @@ winget install --id Playnite.Playnite --source winget
 
 # Messaging and contact applications
 winget install --id Discord.Discord --source winget
+
+# Monitoring and support apps
+winget install --id Guru3D.RTSS --source winget
+winget install --id REALiX.HWiNFO --source winget
+
+# Put links for apps to download that can't be installed with winget
+Write-Warning "THESE LINKS MUST BE FOLLOWED MANUALLY AS THEY ARE NOT IN WINGET"
+Write-Output "NVIDIA APP: https://www.nvidia.com/en-eu/software/nvidia-app/"
+Write-Output "Ryzen Master: https://www.amd.com/en/products/software/ryzen-master.html"
+Write-Output "Trust GXT 155 Mouse: https://support.trust.com/en/support/solutions/articles/9000240828-gxt-155-caldor-gaming-mouse-black-20411"
