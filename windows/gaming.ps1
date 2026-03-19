283d0b3cbe9f5ebfe83e8fc25b62dce4d6016759
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-03-19 10:34:28 +0100

    feat: some basic gaming launchers

---
+++ windows/gaming.ps1
@@ -0,0 +1,9 @@
+# Game launcher platforms
+winget install --id Valve.Steam --source winget
+winget install --id EpicGames.EpicGamesLauncher --source winget
+winget install --id GOG.Galaxy --source winget
+winget install --id Blizzard.BattleNet --source winget
+winget install --id Playnite.Playnite --source winget
+
+# Messaging and contact applications
+winget install --id Discord.Discord --source winget
