377472b710b286c48166319876e8ff9df1b86ccf
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-03-19 10:34:18 +0100

    feat: basic apps and antigravity ide

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,6 +5,11 @@ winget install --id Adobe.Acrobat.Reader.64-bit --source winget
 winget install --id Bitwarden.Bitwarden --source winget
 winget install --id 7zip.7zip --source winget
 winget install --id Greenshot.Greenshot --source winget
+winget install --id OBSProject.OBSStudio --source winget
+winget install --id Spotify.Spotify --source winget
+
+# Microsoft suite
+winget install --id Microsoft.Teams --source winget
 
 # Research
 winget install --id JabRef.JabRef --source winget
@@ -16,9 +21,13 @@ winget install --id Git.Git --source winget
 winget install --id Docker.DockerDesktop --source winget
 winget install --id Microsoft.VisualStudioCode --source winget
 winget install --id Bruno.Bruno --source winget
+winget install --id Google.Antigravity --source winget
 winget install --id MongoDB.Compass.Full --source winget
 winget install --id JGraph.Draw --source winget
 winget install --id EclipseAdoptium.Temurin.25.JDK --source winget
+
+# Messaging apps
+winget install --id Telegram.TelegramDesktop --source winget
 
 # Setup posh git to view diffs on branches
 Install-Module posh-git -Scope CurrentUser
