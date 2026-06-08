b880618d8e7a1e70ffc6a935096339cf281d5bed
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-06-08 19:47:16 +0200

    fix: comments

--- linux/setup.sh
+++ linux/setup.sh
@@ -53,82 +53,82 @@ apt-get update
 chmod +x ./linux/installation_scripts/*.sh
 chmod +x ./linux/*.sh
 
-# # Install basic dependencies for linux
-# apt-get install -y ca-certificates \
-#     curl \
-#     apt-transport-https \
-#     binutils \
-#     gnome-terminal \
-#     network-manager-openconnect \
-#     network-manager-openconnect-gnome \
-#     flatpak \
-#     cmake
-# curl -sL https://raw.githubusercontent.com/retorquere/zotero-pkg/master/install.sh | bash
-# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-# snap install gedit
+# Install basic dependencies for linux
+apt-get install -y ca-certificates \
+    curl \
+    apt-transport-https \
+    binutils \
+    gnome-terminal \
+    network-manager-openconnect \
+    network-manager-openconnect-gnome \
+    flatpak \
+    cmake
+curl -sL https://raw.githubusercontent.com/retorquere/zotero-pkg/master/install.sh | bash
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+snap install gedit
 
-# # Install basic software dependencies
-# snap install bitwarden
-# snap install vivaldi
-# snap install thunderbird
+# Install basic software dependencies
+snap install bitwarden
+snap install vivaldi
+snap install thunderbird
 
-# # Entertainment
-# snap install spotify
+# Entertainment
+snap install spotify
 
-# # Writing and office apps
-# snap install libreoffice
+# Writing and office apps
+snap install libreoffice
 
-# # Research
-# curl -fsSL https://tailscale.com/install.sh | bash
-# apt-get install -y zotero
-# apt-get install -y filezilla
+# Research
+curl -fsSL https://tailscale.com/install.sh | bash
+apt-get install -y zotero
+apt-get install -y filezilla
 
-# # Development
-# apt-get install -y git
-# bash ./linux/installation_scripts/docker.sh
-# snap install code --classic
+# Development
+apt-get install -y git
+bash ./linux/installation_scripts/docker.sh
+snap install code --classic
 
-# # Messaging apps
-# snap install telegram-desktop
+# Messaging apps
+snap install telegram-desktop
 
-# # Windows execution of apps
-# flatpak install flathub com.usebottles.bottles
+# Windows execution of apps
+flatpak install flathub com.usebottles.bottles
 
-# # Install uv for python
-# curl -LsSf https://astral.sh/uv/install.sh | bash
+# Install uv for python
+curl -LsSf https://astral.sh/uv/install.sh | bash
 
-# # Work profile
-# if [[ $WORK -eq 1 ]]; then
-#     bash ./linux/work.sh
-# fi
+# Work profile
+if [[ $WORK -eq 1 ]]; then
+    bash ./linux/work.sh
+fi
 
-# # Give docker permissions to the user
-# groupadd docker
-# usermod -aG docker $SUDO_USER
-# chown root:docker /var/run/docker.sock
+# Give docker permissions to the user
+groupadd docker
+usermod -aG docker $SUDO_USER
+chown root:docker /var/run/docker.sock
 
-# # Manage apps that are not shipped either with snap or apt
-# echo "=============================="
-# echo "Creating programs directory..."
-# if [ -d "/home/$SUDO_USER/programs" ]; then
-#     echo "Directory already exists. Skipping creation."
-# else
-#     mkdir /home/$SUDO_USER/programs
-#     echo "Directory created successfully."
-# fi
+# Manage apps that are not shipped either with snap or apt
+echo "=============================="
+echo "Creating programs directory..."
+if [ -d "/home/$SUDO_USER/programs" ]; then
+    echo "Directory already exists. Skipping creation."
+else
+    mkdir /home/$SUDO_USER/programs
+    echo "Directory created successfully."
+fi
 
-# # Discord
-# echo "=============================="
-# echo "Setting up Discord updater..."
-# cp -p ./linux/installation_scripts/discord.sh /home/$SUDO_USER/programs/discord.sh
-# sed "s/__USER__/$SUDO_USER/g" ./linux/autostart/discord-updater.desktop > "/home/$SUDO_USER/.config/autostart/discord-updater.desktop"
+# Discord
+echo "=============================="
+echo "Setting up Discord updater..."
+cp -p ./linux/installation_scripts/discord.sh /home/$SUDO_USER/programs/discord.sh
+sed "s/__USER__/$SUDO_USER/g" ./linux/autostart/discord-updater.desktop > "/home/$SUDO_USER/.config/autostart/discord-updater.desktop"
 
-# # Kdrive
-# echo "=============================="
-# echo "Setting up Kdrive cloud..."
-# bash ./linux/installation_scripts/kdrive.sh
-# sed "s/__USER__/$SUDO_USER/g" ./linux/applications/kdrive.desktop > "/home/$SUDO_USER/.local/share/applications/kdrive.desktop"
-# apt install -y gnome-shell-extension-appindicator
+# Kdrive
+echo "=============================="
+echo "Setting up Kdrive cloud..."
+bash ./linux/installation_scripts/kdrive.sh
+sed "s/__USER__/$SUDO_USER/g" ./linux/applications/kdrive.desktop > "/home/$SUDO_USER/.local/share/applications/kdrive.desktop"
+apt install -y gnome-shell-extension-appindicator
 
 # Isaac Sim App Launcher
 if [[ $ROBOTICS -eq 1 ]]; then
@@ -138,12 +138,12 @@ if [[ $ROBOTICS -eq 1 ]]; then
     sed "s/__USER__/$SUDO_USER/g" ./linux/applications/isaac-sim.desktop > "/home/$SUDO_USER/.local/share/applications/isaac-sim.desktop"
 fi
 
-# # AnyDesk
-# echo "=============================="
-# echo "Setting up AnyDesk..."
-# install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
-# chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
-# echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
-# apt update
-# apt install -y anydesk
+# AnyDesk
+echo "=============================="
+echo "Setting up AnyDesk..."
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
+apt update
+apt install -y anydesk
