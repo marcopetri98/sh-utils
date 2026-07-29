7ab2089e57258b4cd2db6cad02d68529876c5596
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-07-29 18:36:20 +0200

    feat: ros2 navigation and isaacsim

--- linux/ros2.sh
+++ linux/ros2.sh
@@ -48,8 +48,36 @@ sudo apt upgrade
 sudo apt install ros-jazzy-desktop
 sudo apt install ros-jazzy-ros-base
 sudo apt install ros-dev-tools
+sudo apt install ros-jazzy-vision-msgs
+sudo apt install ros-jazzy-ackermann-msgs
 
 # ==============================================================================
-# STAGE 4: Setup environment
+# STAGE 4: Install Nav2
 # ==============================================================================
 source /opt/ros/jazzy/setup.bash
+sudo apt install ros-jazzy-navigation2
+sudo apt install ros-jazzy-nav2-bringup
+sudo apt install ros-jazzy-nav2-minimal-tb*
+sudo apt install ros-jazzy-turtlebot3-gazebo
+
+# ==============================================================================
+# STAGE 5: Setup ROS environment for Isaac Sim
+# ==============================================================================
+git clone git@github.com:isaac-sim/IsaacSim-ros_workspaces.git
+sudo mv IsaacSim-ros_workspaces/ /home/$USER/programs
+
+sudo apt install python3-rosdep build-essential
+sudo apt install python3-colcon-common-extensions
+
+cd /home/$USER/programs/IsaacSim-ros_workspaces
+git submodule update --init --recursive
+cd jazzy_ws
+sudo rosdep init
+sudo rosdep fix-permissions
+rosdep update
+
+# Ensure rosbag can break everything, otherwise it complains
+PIP_BREAK_SYSTEM_PACKAGES=1 rosdep install -i --from-path src --rosdistro jazzy -y
+
+cd ..
+colcon build --cmake-args -DPython3_EXECUTABLE=/usr/bin/python3
